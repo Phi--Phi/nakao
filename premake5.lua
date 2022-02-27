b7c26b013d06c319454be626557126c1345997aa
Phi--Phi <Phi--Phi@users.noreply.github.com>
2022-02-26 21:59:28 -0500

    added imgui

---
+++ premake5.lua
@@ -0,0 +1,58 @@
+workspace "nakao"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+IncludeDir = {}
+IncludeDir["imgui"] = "vendor/imgui/"
+
+include "vendor/imgui"
+
+project "nakao"
+	location "nakao"
+	kind "ConsoleApp"
+	language "C++"
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("intermediates/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+		"%{prj.name}/src/**.hpp",
+		"%{prj.name}/src/**.c",
+	}
+
+	includedirs
+	{
+		"%{IncludeDir.imgui}"
+	}
+
+	links
+	{
+		"ImGui"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"NAKAO_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "NAKAO_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "NAKAO_RELEASE"
+		optimize "On"
